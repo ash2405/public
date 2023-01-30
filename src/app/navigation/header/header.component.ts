c4d2f777cdfa4e45a64e6bd0491171f4820aa280
ashish <ashish.rathore@calsoftinc.com>
2023-01-30 14:51:56 +0530

    add form tag in header component.

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core'
 import { environment } from 'src/environment/environment'
-
+import { FormGroup,FormBuilder } from "@angular/forms";
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  appConstant: any
+  appConstant: any;
+  fg : FormGroup;
+  constructor(private fb:FormBuilder){
+    this.fg=fb.group({
+      name:['']
+    })
+  }
   ngOnInit(): void { this.appConstant = environment}
 }
--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HeaderComponent } from './header/header.component'
+import { ReactiveFormsModule } from '@angular/forms'
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule,ReactiveFormsModule],
   exports:[HeaderComponent]
 })
 export class NavigationModule {}
